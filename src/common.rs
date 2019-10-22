fffa5bbed7d56e015fc1babacc238bf8c1ab7358
mswift42 <martin.haesler@gmail.com>
2019-10-22 15:02:05 +0100

    check for hex color length of 3.

--- src/common.rs
+++ src/common.rs
@@ -1,5 +1,6 @@
 use palette::Srgb;
 use std::str::FromStr;
+
 pub struct ThemeMap {
     pub dark_bg: bool,
     pub fg1: Srgb,
@@ -35,39 +36,53 @@ impl ThemeColor {
 //    pub fn from_hex(scol: String) -> ThemeColor {
 //    }
 }
+
 impl FromStr for ThemeColor {
     type Err = std::num::ParseIntError;
 
     fn from_str(hex_code: &str) -> Result<Self, Self::Err> {
-        let r: f32 = f32::from_str_radix(&hex_code[1..3], 16)?;
-        let g: f32 = f32::from_str_radix(&hex_code[3..5], 16)?;
-        let b: f32 = f32::from_str_radix(&hex_code[5..7], 16)?;
+        let mut factor = 1.0 / 255.0;
+        let mut r: u8 = u8::from_str_radix(&hex_code[1..3], 16)?;
+        let mut g: u8 = u8::from_str_radix(&hex_code[3..5], 16)?;
+        let mut b: u8 = u8::from_str_radix(&hex_code[5..7], 16)?;
 
-        let col = Srgb::new(r,g,b);
+        if hex_code.len() == 4 {
+            factor = 1.0 / 15.0;
+            r = u8::from_str_radix(&hex_code[1..2], 8)?;
+            g = u8::from_str_radix(&hex_code[2..3], 8)?;
+            b = u8::from_str_radix(&hex_code[3..4], 8)?;
+        }
 
-        Ok(ThemeColor{col})
+        let col = Srgb::new(r as f32 * factor,
+                            g as f32 * factor,
+                            b as f32 * factor);
 
+        Ok(ThemeColor { col })
     }
 }
-fn from_hex(scol: &str) -> (u8,u8, u8) {
-    let format = "#{02x}{02x}{02x}";
-    if scol.len() == 4 {
-        let format = "#{1x}{1x}{1x}";
-    }
-    let (r,g,b) = scan_fmt!(scol, format, [hex u8], [hex u8], [hex u8]).unwrap();
-    (r,g,b)
-}
+
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
-    fn test_from_hex() {
-        let hex1 = "#ffffff";
-        assert_eq!(from_hex(hex1), (255,255,255));
-        let hex1 = "#000000";
-        assert_eq!(from_hex(hex1), (0,0,0));
-        let hex1 = "#fafafa";
-        assert_eq!(from_hex(hex1), (250,250,250));
+    fn from_str() {
+        let hex = "#ffffff";
+        let tc = ThemeColor::from_str("#ffffff");
+        assert!(tc.is_ok());
+        let col = tc.unwrap().col;
+        assert_eq!(col.red, 1.0);
+        assert_eq!(col.green, 1.0);
+        assert_eq!(col.blue, 1.0);
+        let hex = "#000000";
+        let tc = ThemeColor::from_str("#000000");
+        let col = tc.unwrap().col;
+        assert_eq!(col.red, 0.0);
+        assert_eq!(col.green, 0.0);
+        assert_eq!(col.blue, 0.0);
+        let hex = "#gggggg";
+        let tc = ThemeColor::from_str(hex);
+        assert!(tc.is_err());
     }
 }
