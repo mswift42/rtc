2aec438cd627261c745f1a3d002b7808d78e929f
mswift42 <martin.haesler@gmail.com>
2019-10-28 05:38:26 +0000

    ignore non valid hex code input.

--- src/common.rs
+++ src/common.rs
@@ -1,6 +1,8 @@
 use palette::{Srgb, Component};
 use std::str::FromStr;
 use palette::rgb::{Rgb, RgbStandard};
+use std::num::ParseIntError;
+use std::error::Error;
 
 pub struct ThemeMap {
     pub dark_bg: bool,
@@ -42,32 +44,27 @@ impl FromStr for ThemeColor {
     type Err = std::num::ParseIntError;
 
     fn from_str(hex_code: &str) -> Result<Self, Self::Err> {
-        match hex_code.len() {
-            4 | 7 => {
-                let (red, green, blue, factor) = if hex_code.len() == 7 {
-                    (u8::from_str_radix(&hex_code[1..3], 16)?,
-                     u8::from_str_radix(&hex_code[3..5], 16)?,
-                     u8::from_str_radix(&hex_code[5..7], 16)?,
-                     1.0 / 255.0
-                    )
-                } else {
-                    (u8::from_str_radix(&&hex_code[1..2], 16)?,
-                     u8::from_str_radix(&hex_code[2..3], 16)?,
-                     u8::from_str_radix(&hex_code[3..4], 16)?,
-                     1.0 / 15.0
-                    )
-                };
+        let (red, green, blue, factor) = if hex_code.len() == 7 {
+            (u8::from_str_radix(&hex_code[1..3], 16)?,
+             u8::from_str_radix(&hex_code[3..5], 16)?,
+             u8::from_str_radix(&hex_code[5..7], 16)?,
+             1.0 / 255.0
+            )
+        } else {
+            (u8::from_str_radix(&&hex_code[1..2], 16)?,
+             u8::from_str_radix(&hex_code[2..3], 16)?,
+             u8::from_str_radix(&hex_code[3..4], 16)?,
+             1.0 / 15.0
+            )
+        };
 
-                let col = Srgb::new(
-                    red as f32 * factor,
-                    green as f32 * factor,
-                    blue as f32 * factor,
-                );
+        let col = Srgb::new(
+            red as f32 * factor,
+            green as f32 * factor,
+            blue as f32 * factor,
+        );
 
-                Ok(ThemeColor { col })
-            }
-            _ => Ok(ThemeColor {col: u8::
-        }
+        Ok(ThemeColor { col })
     }
 }
 
@@ -137,8 +134,6 @@ mod test {
         assert_eq!((g * 255.0).round() as u8, 34);
         assert_eq!((b * 255.0).round() as u8, 51);
         let hex = "hello";
-        assert!(ThemeColor::from_str(hex).is_err());
-        let hex = "#ff";
         assert!(ThemeColor::from_str(hex).is_err());
     }
 }
