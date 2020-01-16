7ad7e9ba6a023242ef151b1bef5e78e16732ee3b
mswift42 <martin.haesler@gmail.com>
2020-01-16 14:03:04 +0000

    add commentsection.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
 mod common;
+
 extern crate approx;
 extern crate pest;
 #[macro_use]
 extern crate pest_derive;
+
 use common::ThemeColor;
 use std::str::FromStr;
 use pest::Parser;
-#[derive(Parser)]
-#[grammar = "blanktheme.pest"]
-
 use std::collections::HashMap;
 use std::fs;
-use term::Attr::Blink;
 
+#[derive(Parser)]
+#[grammar = "blanktheme.pest"]
 pub struct BlankParser;
+
 fn main() {
     println!("Hello, world!");
     let a = ThemeColor::from_str("#ece3db");
@@ -22,10 +23,9 @@ fn main() {
     println!("{:?}", b.unwrap().to_hex());
     let c = ThemeColor::from_str("");
     println!("{}", c.err().unwrap());
-    let f = fs::read_to_string("themefiles/blank.theme")
+    let f = fs::read_to_string("src/themefiles/blank.theme")
         .expect("could not read file.");
     let file = BlankParser::parse(Rule::file, &f)
         .expect("unsuccesful parse")
         .next().unwrap();
-    
 }
