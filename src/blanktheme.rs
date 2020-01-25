13bcde8802593a6dff7b73304cfa6eeb738121c9
mswift42 <martin.haesler@gmail.com>
2020-01-25 15:58:13 +0000

    return hashmap in from_json method.

--- src/blanktheme.rs
+++ src/blanktheme.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 use std::collections::HashMap;
 use crate::common::ThemeColor;
+use core::str::FromStr;
 use serde::Deserialize;
 use serde_json::Result;
 
@@ -23,9 +24,19 @@ pub struct JsonTheme {
     pub warning2: String,
 }
 
-pub fn from_json(cm: &str) -> HashMap<&str, ThemeColor> {
+pub fn from_json(cm: &str) -> HashMap<&str, String> {
     let parsed: JsonTheme = serde_json::from_str(cm).unwrap();
-    let map: HashMap<&str,ThemeColor> = HashMap::new();
-    println!("{:?}", parsed);
+    let mut map: HashMap<&str,String> = HashMap::new();
+    map.insert("background", parsed.background);
+    map.insert("foreground", parsed.foreground);
+    map.insert("keyword", parsed.keyword);
+    map.insert("function", parsed.function);
+    map.insert("comment", parsed.comment);
+    map.insert("string", parsed.string);
+    map.insert("typeface", parsed.typeface);
+    map.insert("builtin", parsed.builtin);
+    map.insert("constant", parsed.constant);
+    map.insert("warning", parsed.warning);
+    map.insert("warning2", parsed.warning2);
     map
 }
